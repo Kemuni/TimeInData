747d773612de4ed78f29b736d72a28fa3020caaa
JFurB <dimabondarenko471@gmail.com>
2025-04-05 22:41:05 +0300

    Frontend: Fix bug when we have nothing to fill with activities. Now you can save new activities via main Button in telegram. Change background color

--- frontend_service/src/components/App.tsx
+++ frontend_service/src/components/App.tsx
@@ -3,10 +3,16 @@ import { AppRoot } from '@telegram-apps/telegram-ui';
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
 
 import { routes } from '@/navigation/routes.tsx';
+import {getTelegramData} from "@telegram-apps/telegram-ui/dist/helpers/telegram";
 
 export function App() {
   const lp = useLaunchParams();
   const isDark = useSignal(miniApp.isDark);
+
+  const tgData = getTelegramData();
+  if (tgData) {
+    tgData.setBackgroundColor(tgData.themeParams.secondary_bg_color);
+  }
 
   return (
     <AppRoot
---
+++ frontend_service/src/components/icons/Icon24Checkmark.tsx
@@ -0,0 +1,9 @@
+import { SVGProps } from "react";
+
+export const Icon24Checkmark = ({ ...restProps }: SVGProps<SVGSVGElement>) => (
+  <svg width="24" height="24" viewBox="0 -1.5 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" {...restProps}>
+    <path fillRule="evenodd" clipRule="evenodd"
+          d="M3.06066 4.9393C2.47487 4.3536 1.52513 4.3536 0.93934 4.9393C0.35355 5.5251 0.35355 6.4749 0.93934 7.0607L3.93934 10.0607C4.52513 10.6464 5.4749 10.6464 6.0607 10.0607L13.0607 3.0607C13.6464 2.47487 13.6464 1.52513 13.0607 0.93934C12.4749 0.35355 11.5251 0.35355 10.9393 0.93934L5 6.8787L3.06066 4.9393z"
+          fill="currentColor"/>
+  </svg>
+);
