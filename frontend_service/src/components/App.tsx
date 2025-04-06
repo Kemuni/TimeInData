9008b9c1dddec2ff2a5e2184f456ebfc77ed90fc
JFurB <dimabondarenko471@gmail.com>
2025-04-06 20:21:40 +0300

    Frontend: Update dependencies to new versions and refactor code.

--- frontend_service/src/components/App.tsx
+++ frontend_service/src/components/App.tsx
@@ -1,28 +1,23 @@
-import { useLaunchParams, miniApp, useSignal } from '@telegram-apps/sdk-react';
-import { AppRoot } from '@telegram-apps/telegram-ui';
+import { useMemo } from 'react';
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
+import { retrieveLaunchParams, useSignal, isMiniAppDark } from '@telegram-apps/sdk-react';
+import { AppRoot } from '@telegram-apps/telegram-ui';
 
 import { routes } from '@/navigation/routes.tsx';
-import {getTelegramData} from "@telegram-apps/telegram-ui/dist/helpers/telegram";
 
 export function App() {
-  const lp = useLaunchParams();
-  const isDark = useSignal(miniApp.isDark);
-
-  const tgData = getTelegramData();
-  if (tgData) {
-    tgData.setBackgroundColor(tgData.themeParams.secondary_bg_color);
-  }
+  const lp = useMemo(() => retrieveLaunchParams(), []);
+  const isDark = useSignal(isMiniAppDark);
 
   return (
     <AppRoot
       appearance={isDark ? 'dark' : 'light'}
-      platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
+      platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
     >
       <HashRouter>
         <Routes>
           {routes.map((route) => <Route key={route.path} {...route} />)}
-          <Route path="*" element={<Navigate to="/" />}/>
+          <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
       </HashRouter>
     </AppRoot>
