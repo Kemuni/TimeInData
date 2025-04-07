5f04105c882547bba2c7fe27c622eeaffb97432f
JFurB <dimabondarenko471@gmail.com>
2025-04-07 23:08:45 +0300

    Frontend: Rename env variable of API domain

--- frontend_service/src/services/api-urls.ts
+++ frontend_service/src/services/api-urls.ts
@@ -1,4 +1,4 @@
-const baseURL: string = import.meta.env.VITE_API_API_DOMAIN!;
+const baseURL: string = import.meta.env.VITE_API_DOMAIN!;
 
 export class APIEndpointsUrls {
   public static GetUserLastActivity = (userId: number): string => `${baseURL}/users/${userId}/activities/last`;
