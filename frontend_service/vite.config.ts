59b75afe5bc1b8df409cf41c58f1a8b077cd0099
JFurB <dimabondarenko471@gmail.com>
2025-04-07 13:46:47 +0300

    Fix restart protocol of celery worker. Update Dockerfile for new tg web app

--- frontend_service/vite.config.ts
+++ frontend_service/vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
+export default defineConfig(() => {
+
+  return {
+    plugins: [react()],
+    build: { target: 'esnext', emptyOutDir: true, },
+    publicDir: './public',
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
   }
-})
+});
