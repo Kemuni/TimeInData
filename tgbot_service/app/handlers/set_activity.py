351fdefbf0252d2ceb5259be460345e8c6000d68
JFurB <dimabondarenko471@gmail.com>
2026-01-10 20:06:08 +0300

    TGBOT: Fix set_activity for new ActivitySlotsController and SlotsControllerAdapter

--- tgbot_service/app/handlers/set_activity.py
+++ tgbot_service/app/handlers/set_activity.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta
-from typing import Dict, Any, List, Optional, Union, Iterator
+from typing import Dict, Any, Iterable
 
 from aiogram import Router, F
 from aiogram import types
@@ -9,10 +8,19 @@ from aiogram_dialog import DialogManager, Dialog, Window, StartMode
 from aiogram_dialog.widgets.input import MessageInput
 from aiogram_dialog.widgets.kbd import Cancel, Checkbox
 from aiogram_dialog.widgets.text import Const, Format
+from loguru import logger
 
 from app.api_parser import APIParser
-from app.api_parser.types import ActivityBaseIn, ActivityTypes, Activity
+from app.exceptions import ActivitySlotsAdapterException
+from app.exceptions import (
+    NotFoundActivitySlotException,
+    ActivitySlotAlreadyHasActivityException,
+    ActivitySlotsControllerInitException
+)
 from app.states.set_activity import SetActivityDialogSG
+from app.types import ActivityBaseIn, ActivityTypes
+from app.utils.activity_slots_controller import ActivitySlotsController
+from app.utils.slots_controller_adapter import ActivitySlotsToAiogramDialogAdapter
 
 
 class ActivityFormatError(Exception):
@@ -23,24 +31,38 @@ SHOW_ACTIVITIES_BTN_ID = "show_activities_btn"
 AVAILABLE_ACTIVITIES_STR = ", ".join(i.name.lower() for i in ActivityTypes)
 
 
-async def getter(dialog_manager: DialogManager, **kwargs) -> Dict[str, Any]:
+async def getter(dialog_manager: DialogManager, **_) -> Dict[str, Any]:
+    try:
+        slots_controller = ActivitySlotsToAiogramDialogAdapter.get_controller_from_dialog_data(dialog_manager)
+    except ActivitySlotsAdapterException as exc:
+        logger.error(f"Failed to get activity slots controller from dialog data. Error: {exc}")
+        await dialog_manager.done()
+        return {}
+
+    missing_slots_str = ""
+    for filtered_slots in slots_controller.get_slots_filtered_by_date():
+        activities_str_hours = (f'{slot.local_hour:02d}:00' for slot in filtered_slots.activity_slots)
+        missing_slots_str += f"<b>{filtered_slots.local_date.strftime("%d %B")}</b>: {', '.join(activities_str_hours)}"
     return {
         SHOW_ACTIVITIES_BTN_ID: dialog_manager.find(SHOW_ACTIVITIES_BTN_ID).is_checked(),
-        'hours_to_submit_str': ", ".join(
-            str(hour) + ':00' for hour in dialog_manager.dialog_data['hours_to_submit']
-        ),
+        'missing_slots_str': missing_slots_str,
     }
 
 
-def parse_hours_range(hour_str: str) -> Union[Iterator[int], range]:
+def parse_hours_range(hour_str: str) -> Iterable[int]:
     """
     Parse hours range from text format.
-    For example: hour_str="12-15" -> return range(12, 16); "12" -> return [12]; "23-3" -> return [23, 00, 01, 02]
+    For example: hour_str="12-14" -> return [12, 13, 14]; "12" -> return [12]; "23-3" -> return [23, 00, 01, 02, 03];
     :param hour_str: String format of hour. Example: "12-15" or "12".
-    :return: Range or iterator of hours.
+    :return: Iterable[int] of hours.
     """
     if '-' not in hour_str:
-        hour = int(hour_str)
+        try:
+            hour = int(hour_str)
+        except ValueError:
+            raise ActivityFormatError(
+                f'⚠️ "{hour_str}" must be a number from 0 to 23. Try again!'
+            )
         if hour >= 24:
             raise ActivityFormatError(
                 f'⚠️ Hour cannot be more than 23. Try again!'
@@ -60,123 +82,87 @@ def parse_hours_range(hour_str: str) -> Union[Iterator[int], range]:
         )
 
     if from_hour > to_hour:  # "23-3" = [23, 00, 01, 02]
-        return (i % 24 for i in range(from_hour, 24 + to_hour + 1))
-    return range(from_hour, to_hour + 1)  # "13-16" = [13, 14, 15]
+        to_hour += 24
+    return ((from_hour + i) % 24 for i in range(to_hour - from_hour + 1)) # "13-16" = [13, 14, 15]
 
 
-def parse_activity_from_string(
-        activity_row: str, hours_to_submit: set[int], tz_delta: int, start_date: datetime
-) -> List[ActivityBaseIn]:
+def parse_activity_from_string(activity_data_str: str, slots_controller: ActivitySlotsController) -> None:
     """
-    Parse activity from hour and activity string format. Return list of activities objects and new list of hours to
-    submit.
+    Parse activity from hour and activity string format. Add activity to slots controller.
 
-    :param activity_row: String in format like "<time> <activity_type>" or "<from_time>-<to_time> <activity_type>",
-    :param hours_to_submit: Hours, that user have to submit with activity.
-    :param tz_delta: User's delta of time zone (i.e. UTC+3 = 3, UTC-2 = -2).
-    :param start_date: Datetime when user start dialog and our program calculate `hours_to_submit`.
-    :return: Return list of activities objects and new list of hours to submit.
+    :param activity_data_str: String in format like "<time> <activity_type>" or "<from_time>-<to_time> <activity_type>",
+    :param slots_controller: ActivitySlotsController instance.
     """
     # Validate and parse data from text given
     try:
-        hour_str: str
-        activity: str
-        hour_str, activity = activity_row.split()
+        hour_str, activity_str = activity_data_str.split()
     except ValueError:
         raise ActivityFormatError('⚠️ You have written your message in wrong format. Try again!')
 
-    activity: str = activity.strip()
-    if not hasattr(ActivityTypes, activity.upper()):
+    try:
+        activity = ActivityTypes(activity_str.strip().upper())
+    except ValueError:
         raise ActivityFormatError(
-            f'⚠️ The activity "{activity}" does not exist.\n'
+            f'⚠️ The activity "{activity_str}" does not exist.\n'
             f'Available activities: {AVAILABLE_ACTIVITIES_STR}.\n'
             f'Try again!"'
         )
 
-    # Create new activities objects
-    start_date = datetime(start_date.year, start_date.month, start_date.day, start_date.hour)
-    tz_start_date = start_date + timedelta(hours=tz_delta)
-    activities: List[ActivityBaseIn] = []
+    # Add new activities objects
     for hour in parse_hours_range(hour_str):
-        if hour not in hours_to_submit:
+        try:
+            slots_controller.add_activity_slot(hour, activity)
+        except NotFoundActivitySlotException:
             raise ActivityFormatError(
-                f"⚠️ You don't need to set activity for {hour} hour or you have already set it. Try again!"
+                f"⚠️ You don't need to set activity for {hour:02d}:00 hour. Try again!"
+            )
+        except ActivitySlotAlreadyHasActivityException:
+            raise ActivityFormatError(
+                f"⚠️ You have already set activity for {hour} hour. Try again!"
             )
 
-        hours_to_submit.remove(hour)
-        hours_gap = (24 - hour + tz_start_date.hour) if hour >= tz_start_date.hour else tz_start_date.hour - hour
-        activities.append(
-            ActivityBaseIn(
-                type=ActivityTypes[activity.upper()].value,
-                time=(start_date - timedelta(hours=hours_gap)).strftime(APIParser.DATETIME_FORMAT),  # To UTC format
-            )
-        )
 
-    return activities
-
-
-async def process_message(message: types.Message, _, manager: DialogManager):
+async def process_message(message: types.Message, _, manager: DialogManager) -> None:
     """ Get message and create activity from data given in user's message """
-    # Validating user message and converting it into a list of activities
-    hours_to_submit: set[int] = set(manager.dialog_data['hours_to_submit'])
-    tz_delta: int = manager.dialog_data['tz_delta']
-    start_date: datetime = manager.dialog_data['start_date']
+    # Validating the user message and converting it into a list of activities
+    try:
+        slots_controller = ActivitySlotsToAiogramDialogAdapter.get_controller_from_dialog_data(manager)
+    except ActivitySlotsAdapterException as exc:
+        logger.error(f"Failed to get activity slots controller from dialog data. Error: {exc}")
+        await manager.done()
+        return
 
-    activities: List[ActivityBaseIn] = []
     activity_items = message.text.strip().split('\n')
     try:
         for activity_row in activity_items:
-            new_activities = parse_activity_from_string(activity_row, hours_to_submit, tz_delta, start_date)
-            activities.extend(new_activities)
+            parse_activity_from_string(activity_row, slots_controller)
     except ActivityFormatError as e:
         await message.reply(str(e))
         return
 
-    if hours_to_submit:
-        await message.reply('⚠️ You have to set activity for all hours. Try again!')
+    if not slots_controller.has_all_slots_filled():
+        await message.reply('⚠️ You have to set activity for ALL specified hours. Try again!')
         return
 
     # Sending user's activities to our API service
     api: APIParser = manager.middleware_data['api']
-    await api.add_user_activities(message.from_user.id, activities)
+    activities_for_api = [
+        ActivityBaseIn(utc_date=slot.utc_date, utc_hour=slot.utc_hour, type=str(slot.activity_type.value))
+        for slot in slots_controller.get_all_slots()
+    ]
+    await api.add_user_activities(message.from_user.id, activities_for_api)
 
     await manager.next()
-
-
-def get_hours_to_submit(last_activity: Optional[Activity] = None, to_date: Optional[datetime] = None) -> list[int]:
-    """
-    Receive last activity object and return list of hours that user can fill via activity.
-    :param last_activity: Object of last user's activity.
-    :param to_date: Deadline date to check hours without activity.
-    :return: List of hours that user can fill via activity.
-    """
-    today = to_date or datetime.utcnow()
-    if last_activity is None:  # If user is new for us - let him fill only today's hour
-        return [hour for hour in range(0, today.hour)]
-
-    # Otherwise, let user set activity to all 24 hours gap, including yesterday
-    last_activity_time = datetime.strptime(last_activity.time, APIParser.DATETIME_FORMAT)
-    if last_activity_time.date() == today.date():
-        return [hour for hour in range(last_activity_time.hour + 1, today.hour)]
-
-    if today.date() - last_activity_time.date() <= timedelta(days=1):
-        from_hour = last_activity_time.hour + 1
-    else:
-        from_hour = today.hour
-
-    return [hour % 24 for hour in range(from_hour, 24 + today.hour)]
 
 
 async def on_start(_, manager: DialogManager) -> None:
     """ Get start data for the dialog """
     api = manager.middleware_data['api']
     tz_delta = await api.get_user_time_zone_delta(manager.event.from_user.id)
-    last_activity = await api.get_user_last_activity(manager.event.from_user.id)
+    api_missing_slots_data = await api.get_closest_activity_missing_slots(manager.event.from_user.id)
 
-    # Provide date to dialog for excepting errors with activity when user start the dialog in time like 17:59
-    utc_today = datetime.utcnow()
-    hours_to_submit = [(hour + tz_delta) % 24 for hour in get_hours_to_submit(last_activity, to_date=utc_today)]
-    if not hours_to_submit:
+    # Provide date to dialog about missing activity slots
+    if not api_missing_slots_data.has_missing_slots:
         if isinstance(manager.event, types.CallbackQuery):
             msg = manager.event.message
         else:
@@ -185,9 +171,19 @@ async def on_start(_, manager: DialogManager) -> None:
         await manager.done()
         return
 
-    manager.dialog_data['hours_to_submit'] = hours_to_submit
-    manager.dialog_data['tz_delta'] = tz_delta
-    manager.dialog_data['start_date'] = utc_today
+    try:
+        slots_controller = ActivitySlotsController(api_missing_slots_data.missing_slots, tz_delta)
+    except ActivitySlotsControllerInitException as exc:
+        logger.error(f'Error on ActivitySlotsController initialization: {exc}')
+        await manager.done()
+        return
+
+    try:
+        ActivitySlotsToAiogramDialogAdapter.add_controller_to_dialog_data(slots_controller, dialog_manager=manager)
+    except ActivitySlotsAdapterException as exc:
+        logger.error(f"Failed to add activity slots controller to dialog_data. Error: {exc}")
+        await manager.done()
+        return
 
 
 dialog = Dialog(
@@ -196,7 +192,7 @@ dialog = Dialog(
             'Now is time to set your activity for last hour(s)! 🕣'
         ),
         Format(
-            '📝 You need to write your activity for {hours_to_submit_str}'
+            '📝 You need to write your activity for next hour-slots: \n{missing_slots_str}'
         ),
         Const(
             f"\n📌 <b>Available activities:</b> {AVAILABLE_ACTIVITIES_STR}",
